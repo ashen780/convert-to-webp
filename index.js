cc3b2770e1f15dc0e20541dad66cd647e305fc89
ashen780 <ashenm780@gmail.com>
2021-08-02 11:59:15 +0530

    error and info login added

--- index.js
+++ index.js
@@ -4,18 +4,33 @@ import imagemin from "imagemin";
 import imageminWebp from "imagemin-webp";
 import fs from "fs";
 import path from "path";
+import log from "simple-node-logger";
 
 const inputFolder = "./ImagesInput";
 const outputFolder = "./ImagesOutput/";
+
+const logInfo = log.createSimpleLogger(`${inputFolder}-info.log`);
+const logError = log.createSimpleLogger(`${inputFolder}-error.log`);
+const testLogger = log.createSimpleLogger(`${inputFolder}-test.log`);
 
 fs.readdir(inputFolder, async (err, files) => {
   for (const file of files) {
     //check extention
     const ext = path.extname(file);
     // console.log(ext);
-    if (ext === ".jpg" || ext === ".png") {
-      const contents = await convert(file);
-      //console.log(contents);
+    if (
+      ext === ".jpg" ||
+      ext === ".png" ||
+      ext === ".webp" ||
+      ext === ".jpeg"
+    ) {
+      try {
+        const contents = await convert(file);
+        //console.log(contents);
+        continue;
+      } catch (error) {
+        continue;
+      }
     }
   }
 });
@@ -32,11 +47,16 @@ const convert = (imageName) => {
     })
       .then((res) => {
         // console.log("Images Converted Successfully!!!");
-        resolve(res);
+        resolve("Successfully!!");
+        console.log("Successfully!!" + Math.floor(Math.random() * 100));
+        //testLogger.info("Successfully filename " + imageName);
       })
       .catch((err) => {
-        reject(err);
-        console.log(err);
+        reject("reject filename " + imageName);
+        //log to file
+        logInfo.info("reject filename " + imageName);
+        logError.info("Error" + err);
+        console.log(err, imageName);
       });
   });
 };
